9634aed84658b13de3d9218c107914400d4f0276
Pedro Julio G <1102291@est.intec.edu.do>
2024-08-06 09:33:26 -0400

    adding files

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('calculator-form').addEventListener('submit', function(event) {
+  event.preventDefault(); // Evita que el formulario se envíe y recargue la página
+
+  var inputData = document.getElementById('initialData').value; // Obtiene el valor del input
+  var result = inputData * 2; 
+
+  document.getElementById('output').textContent = result; // Muestra el resultado en el span con id 'output'
+});
