f5076891b0edc25d887fff8d7d92614c0386a2bb
Pedro Julio G <1102291@est.intec.edu.do>
2024-08-07 14:42:45 -0400

    adding radio button

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 document.getElementById('solar-panel-form').addEventListener('submit', function(event) {
   event.preventDefault(); // Previene la recarga de la página
 
-  const monthlyConsumption = parseFloat(document.getElementById('monthlyConsumption').value); // Asegura que se convierta a número
+  const monthlyConsumption = parseFloat(document.getElementById('monthlyConsumption').value);
+  const panelPowerRadios = document.getElementsByName('panelPower');
+  let powerPerPanel;
+  
+  // Recorre los botones de radio para encontrar el seleccionado
+  for (let i = 0; i < panelPowerRadios.length; i++) {
+      if (panelPowerRadios[i].checked) {
+          powerPerPanel = parseFloat(panelPowerRadios[i].value); // Asigna el valor seleccionado
+          break;
+      }
+  }
+
   const annualConsumption = monthlyConsumption * 12; // Convierte el consumo mensual en anual
   const dailyConsumption = annualConsumption / 365; // Consumo diario
-  const requiredPower = annualConsumption / 1700; // Potencia necesaria por hora, asumiendo 4.6 horas de sol pico
+  const requiredPower = dailyConsumption / 4.6; // Potencia necesaria por hora, asumiendo 4.6 horas de sol pico
   const requiredPowerW = requiredPower * 1000; // Convertir kW a W
-  const powerPerPanel = 500; // 500W es la potencia de un panel típico
+  const panelsNeeded = Math.round(requiredPowerW / powerPerPanel);
 
-  const panelsNeeded = requiredPowerW / powerPerPanel;
-
-  // Muestra los resultados en la consola para depuración
-  console.log("Consumo Mensual: " + monthlyConsumption + " kWh");
-  console.log("Consumo Anual: " + annualConsumption + " kWh");
-  console.log("Consumo Diario: " + dailyConsumption.toFixed(2) + " kWh");
-  console.log("Potencia Necesaria por Hora: " + requiredPower.toFixed(2) + " kW");
-  console.log("Potencia Necesaria a Utilizar: " + requiredPowerW.toFixed(2) + " W");
-  console.log("Potencia por Panel: " + powerPerPanel + " W");
-  console.log("Paneles Necesarios: " + panelsNeeded.toFixed(2));
-
-  // Muestra el resultado en la página
-  document.getElementById('panelsNeeded').textContent = panelsNeeded.toFixed(2);
+  document.getElementById('panelsNeeded').textContent = panelsNeeded;
+  console.log("Consumo anual: " + annualConsumption + " kWh");
+  console.log("Consumo diario: " + dailyConsumption.toFixed(2) + " kWh");
+  console.log("Potencia necesaria por hora: " + requiredPower.toFixed(2) + " kW");
+  console.log("Potencia por panel: " + powerPerPanel + " W");
+  console.log("Paneles necesarios: " + panelsNeeded);
 });
 
