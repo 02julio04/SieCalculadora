d92292ddc52f32ac7ee2f969f6fb60198ad4e361
Pedro Julio G <1102291@est.intec.edu.do>
2024-08-07 13:38:03 -0400

    Fix formula and styles

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-document.getElementById('calculator-form').addEventListener('submit', function(event) {
-  event.preventDefault(); // Evita que el formulario se envíe y recargue la página
+document.getElementById('solar-panel-form').addEventListener('submit', function(event) {
+  event.preventDefault(); // Previene la recarga de la página
 
-  var inputData = document.getElementById('initialData').value; // Obtiene el valor del input
-  var result = inputData * 2; //formula
+  const monthlyConsumption = parseFloat(document.getElementById('monthlyConsumption').value); // Asegura que se convierta a número
+  const annualConsumption = monthlyConsumption * 12; // Convierte el consumo mensual en anual
+  const dailyConsumption = annualConsumption / 365; // Consumo diario
+  const requiredPower = annualConsumption / 1700; // Potencia necesaria por hora, asumiendo 4.6 horas de sol pico
+  const requiredPowerW = requiredPower * 1000; // Convertir kW a W
+  const powerPerPanel = 500; // 500W es la potencia de un panel típico
 
-  document.getElementById('output').textContent = result; // Muestra el resultado en el span con id 'output'
+  const panelsNeeded = requiredPowerW / powerPerPanel;
+
+  // Muestra los resultados en la consola para depuración
+  console.log("Consumo Mensual: " + monthlyConsumption + " kWh");
+  console.log("Consumo Anual: " + annualConsumption + " kWh");
+  console.log("Consumo Diario: " + dailyConsumption.toFixed(2) + " kWh");
+  console.log("Potencia Necesaria por Hora: " + requiredPower.toFixed(2) + " kW");
+  console.log("Potencia Necesaria a Utilizar: " + requiredPowerW.toFixed(2) + " W");
+  console.log("Potencia por Panel: " + powerPerPanel + " W");
+  console.log("Paneles Necesarios: " + panelsNeeded.toFixed(2));
+
+  // Muestra el resultado en la página
+  document.getElementById('panelsNeeded').textContent = panelsNeeded.toFixed(2);
 });
+
