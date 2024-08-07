22935922cf5906328aef3c7126c1e937c197e050
Pedro Julio G <1102291@est.intec.edu.do>
2024-08-07 09:03:59 -0400

    Refactor Style and contents

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('calculator-form').addEventListener('submit', function(e
   event.preventDefault(); // Evita que el formulario se envíe y recargue la página
 
   var inputData = document.getElementById('initialData').value; // Obtiene el valor del input
-  var result = inputData * 2; 
+  var result = inputData * inputData; //formula
 
   document.getElementById('output').textContent = result; // Muestra el resultado en el span con id 'output'
 });
